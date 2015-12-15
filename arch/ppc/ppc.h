fef72e5312896af05dde652124ff73dfdc0c55c0
Karol Arndt <karol.arndt@gmail.com>
2015-12-15 17:09:30 +0100

    [#35] Remove ppc_tb_t.

--- arch/ppc/ppc.h
+++ arch/ppc/ppc.h
@@ -3,19 +3,6 @@
 #include "cpu.h"
 void ppc_set_irq (CPUState *env, int n_IRQ, int level);
 
-struct ppc_tb_t {
-    /* Time base management */
-    int64_t  tb_offset;    /* Compensation                    */
-    int64_t  atb_offset;   /* Compensation                    */
-    uint32_t tb_freq;      /* TB frequency                    */
-    uint64_t purr_load;
-    uint64_t purr_start;
-    void *opaque;
-    uint32_t flags;
-};
-
-uint64_t cpu_ppc_get_tb(ppc_tb_t *tb_env, uint64_t vmclk, int64_t tb_offset);
-clk_setup_cb cpu_ppc_tb_init (CPUState *env, uint32_t freq);
 /* Embedded PowerPC DCR management */
 typedef uint32_t (*dcr_read_cb)(void *opaque, int dcrn);
 typedef void (*dcr_write_cb)(void *opaque, int dcrn, uint32_t val);
