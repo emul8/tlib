fdeb8362235b1f8937bfaeb154c5c4ea5d8b2535
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2016-12-13 15:55:37 +0100

    [#7070] Remove redundant compile-time check.

--- include/targphys.h
+++ include/targphys.h
@@ -7,7 +7,6 @@
 #define TARGET_PHYS_ADDR_BITS TARGET_LONG_BITS
 #endif
 
-#ifdef TARGET_PHYS_ADDR_BITS
 /* target_phys_addr_t is the type of a physical address (its size can
    be different from 'target_ulong').
 
@@ -28,4 +27,3 @@
 #endif
 #endif
 
-#endif
